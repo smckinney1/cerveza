1aae9369eddf40ec42c6942360d0de8dd71ee973
Shannon McKinney <srmckinn@gmail.com>
2016-04-10 15:14:36 -0400

    Create array and add it to divs in HTML
    Create an array of the idea submissions and add each submission, as it
    is submitted, to a new div in the HTML.

--- cerveza.js
+++ cerveza.js
@@ -1,7 +1,32 @@
+//non-working code
 /*document.getElementById("add-idea-submit").onclick(
 	alert("stop it");
 	)*/
 
+window.onload = function() {
+	document.getElementById("add-idea").focus();
+}
+
+var ideaArray = [];
+
 function fuck() {
 	alert("FUCK YOU!!!");
+	document.getElementById("add-idea").removeAttribute("placeholder"); //see how to add it back when click outside again
 }
+
+function submitArray(event) {
+	event.preventDefault();					//prevent page from refreshing on submit
+	var submission = document.getElementById("add-idea").value;
+	ideaArray.push(submission);
+	console.log(ideaArray);
+	//document.getElementById("idea-array").innerHTML = ideaArray;
+	addArrayItemsToDiv();
+	function addArrayItemsToDiv() {
+		var newDiv = document.createElement("div");
+		var node = document.createTextNode(submission);
+		newDiv.appendChild(node);
+		var element = document.getElementById("idea-array");
+		element.appendChild(newDiv);
+	}
+}
+
