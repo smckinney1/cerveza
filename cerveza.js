f593bfcd0f94c4270145dd5eaed1412fc8a02cc2
Shannon McKinney <srmckinn@gmail.com>
2016-04-17 14:43:19 -0400

    Converted JavaScript to jQuery in JS file

--- cerveza.js
+++ cerveza.js
@@ -1,26 +1,35 @@
-//non-working code
-/*document.getElementById("add-idea-submit").onclick(
-	alert("stop it");
-	)*/
-
-window.onload = function() {
+//JavaScript
+/*window.onload = function() {
 	document.getElementById("add-idea").focus();
-}
+}*/
+
+//jQuery
+$(document).ready(function() {
+	$('#add-idea').focus();
+})
 
 var ideaArray = [];
 
-function fuck() {
+//JavaScript
+/*function fuck() {
 	alert("FUCK YOU!!!");
 	document.getElementById("add-idea").removeAttribute("placeholder"); //see how to add it back when click outside again
-}
+}*/
 
-function submitArray(event) {
+//jQuery - unwanted!!!
+/*function removePlaceholder() {
+	$('#add-idea').removeAttr('placeholder');
+}*/
+
+//JavaScript
+/*function submitArray(event) {
 	event.preventDefault();					//prevent page from refreshing on submit
 	var submission = document.getElementById("add-idea").value;
 	ideaArray.push(submission);
 	console.log(ideaArray);
 	//document.getElementById("idea-array").innerHTML = ideaArray;
 	addArrayItemsToDiv();
+	document.getElementById("add-idea").value = '';				//clears the value entered from the submission
 	function addArrayItemsToDiv() {
 		var newDiv = document.createElement("div");
 		var node = document.createTextNode(submission);
@@ -28,5 +37,28 @@ function submitArray(event) {
 		var element = document.getElementById("idea-array");
 		element.appendChild(newDiv);
 	}
+}*/
+
+//jQuery - original code
+/*function submitArray(event) {
+	event.preventDefault();
+	var submission = $('#add-idea').val();
+	ideaArray.push(submission);
+	//console.log(ideaArray);
+	$('#add-idea').val('');
+	addArrayItemsToDiv();
+	function addArrayItemsToDiv() {
+		$('#idea-array').append('<div class="idea-div"></div>');
+		$('.idea-div').append(submission);
+	}
+}*/
+
+//submitArray optimized
+function submitArray(e) {
+	e.preventDefault();
+	var submission = $('#add-idea').val();
+	ideaArray.push(submission);
+	$('#idea-array').append('<div>' + submission + '</div>');
+	$('#add-idea').val('');
 }
 
